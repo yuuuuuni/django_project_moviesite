aca44b348fe863ae2f4d98ef91b92e9737f038bd
yuuuuuni <yuuuuuni930302@gmail.com>
2022-01-26 13:51:28 +0900

    Question, Answer에 author 속성 추가 완료

--- common/views.py
+++ common/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-
 from common.forms import UserForm
 
 
@@ -12,10 +11,10 @@ def signup(request): # 사용자가 입력한 회원가입 내용
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
+            username = form.cleaned_data.get('username') # form.cleaned_data.get 함수 : 입력값을 개별적으로 얻고 싶은 경우에 사용하는 함수
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password) # 사용자 인증. username, password가 값이 다 맞으면 user에 넣어라
-            login(request, user) # 로그인해라
+            user = authenticate(username=username, password=raw_password) # 사용자 인증. username, password가 값이 다 맞으면 user에 넣어라. authenticate 함수는 사용자명과 비밀번호가 정확한지 검증하는 함수
+            login(request, user) # 로그인
             return redirect('index')
     else:
         form = UserForm()
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -6,6 +7,8 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # 글쓴이 속성 추가
+
 
     def __str__(self):
         return self.subject
@@ -15,6 +18,8 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # 외래키
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # 글쓴이 속성 추가
+
 
     def __str__(self):
         return self.content
