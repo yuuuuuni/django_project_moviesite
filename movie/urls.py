15cfa85680747d5e331abfcd99fc95657a23f022
yuuuuuni <yuuuuuni930302@gmail.com>
2022-02-08 12:35:04 +0900

    3-10 댓글 파트 완료

--- movie/urls.py
+++ movie/urls.py
@@ -14,9 +14,13 @@ urlpatterns = [
     path('question/delete/<int:question_id>/', views.question_delete, name='question_delete'),
     path('answer/modify/<int:answer_id>/', views.answer_modify, name='answer_modify'),
     path('answer/delete/<int:answer_id>/', views.answer_delete, name='answer_delete' ),
-    path('comment/create/question/<int:question_id>/', views.comment_create_question, name='comment_create_question'), # 댓글 등록 시에는 질문의 id번호가 필요
-    path('comment/modify/question/<int:comment_id>/', views.comment_modify_question, name='comment_modify_question'), # 댓글 수정 시에는 댓글의 id번호가 필요
-    path('comment/delete/question/<int:comment_id>/', views.comment_delete_question, name='comment_delete_question'), # 댓글 수정 시에는 댓글의 id번호가 필요
+    path('comment/create/question/<int:question_id>/', views.comment_create_question, name='comment_create_question'), # 질문의 댓글 등록 시에는 질문의 id번호가 필요
+    path('comment/modify/question/<int:comment_id>/', views.comment_modify_question, name='comment_modify_question'), # 질문의 댓글 수정 시에는 댓글의 id번호가 필요
+    path('comment/delete/question/<int:comment_id>/', views.comment_delete_question, name='comment_delete_question'), # 질문의 댓글 삭제 시에는 댓글의 id번호가 필요
+    path('comment/create/answer/<int:answer_id>/', views.comment_create_answer, name='comment_create_answer'), # 답변의 댓글 등록 시에는 답변의 id번호가 필요
+    path('comment/modify/answer/<int:comment_id>/', views.comment_modify_answer, name='comment_modify_answer'), # 답변의 댓글 수정 시에는 댓글의 id번호가 필요
+    path('comment/delete/answer/<int:comment_id>/', views.comment_delete_answer, name='comment_delete_answer'), # 답변의 댓글 삭제 시에는 댓글의 id번호가 필요
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- movie/views.py
+++ movie/views.py
@@ -204,3 +204,61 @@ def comment_delete_question(request, comment_id):
     else:
         comment.delete()
     return redirect('movie:detail', question_id=comment.question.id)
+
+
+@login_required(login_url='common:login')
+def comment_create_answer(request, answer_id):
+    """
+    답변댓글 등록
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.create_date = timezone.now()
+            comment.answer = answer
+            comment.save()
+            return redirect('movie:detail', question_id=comment.answer.question.id)
+    else:
+        form = CommentForm()
+    context = {'form': form}
+    return render(request, 'movie/comment_form.html', context)
+
+
+@login_required(login_url='common:login')
+def comment_modify_answer(request, comment_id):
+    """
+    답변댓글 수정
+    """
+    comment = get_object_or_404(Comment, pk=comment_id) # comment_id를 받아 Comment객체(model에서 정의된)들 중 pk가 comment_id인 것이 있으면 가져오고 그렇지 않으면 404 에러를 발생시켜라. 그것을 comment라는 변수에 담음
+    if request.user != comment.author:
+        messages.error(request, '댓글 수정 권한이 없습니다.')
+        return redirect('movie:detail', question_id=comment.answer.question.id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.modify_date = timezone.now()
+            comment.save()
+            return redirect('movie:detail', question_id=comment.answer.question.id)
+    else:
+        form = CommentForm(instance=comment)
+    context = {'form': form}
+    return render(request, 'movie/comment_form.html', context)
+
+
+@login_required(login_url='common:login')
+def comment_delete_answer(request, comment_id):
+    """
+    답변댓글 삭제
+    """
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user != comment.author:
+        messages.error(request, '댓글 삭제 권한이 없습니다.')
+        return redirect('movie:detail', question_id=comment.answer.question.id)
+    else:
+        comment.delete()
+    return redirect('movie:detail', question_id=comment.answer.question.id)
